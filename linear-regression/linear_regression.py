10e94da5194b72acdd14a1a19501c8dd41dfa440
nikolaykolibarov <nikolaykolibarov@gmail.com>
2020-04-30 20:19:04 +0300

    Initial commit. Implemented Linear Regression.

---
+++ linear-regression/linear_regression.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class LinearRegression:
+    def __init__(self, learning_rate=0.001, n_iterations=1000):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        # init parameters
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
+        for _ in range(self.n_iterations):
+            # approximation
+            y_predicted = np.dot(X, self.weights) + self.bias
+
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            # update parameters
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        y_predicted = np.dot(X, self.weights) + self.bias
+
+        return y_predicted
